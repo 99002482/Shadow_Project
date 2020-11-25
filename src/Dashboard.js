abb073b8d67e87ad590ca957fc84587c1b310d39
99002482 <nisarga.hs@ltts.com>
2020-11-25 16:13:09 +0530

    Making sidebar as different component

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,89 +1,36 @@
 import React, { Component } from 'react';
-import Select from 'react-select';
+import Sidebar from './Sidebar';
 import Header from "./Header";
 import Footer from "./Footer";
 import Cardview from "./Cardview";
-import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 
  
-const options = [
-  { value: 'Mysore', label: 'Mysore' },
-  { value: 'Bangalore', label: 'Bangalore' },
-  { value: 'Mumbai', label: 'Mumbai' },
-];
+
 class Dashboard extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      selectedOption: null,
-      isLoaded:false,
-    }
-    
-  }
-  
-  handleChange = selectedOption => {
-    this.setState({ selectedOption });
-    };
-
-    componentDidMount(){
-      fetch('https://jsonplaceholder.typicode.com/users/1')
-      .then(res=>res.json())
-      .then(json=>{
-          this.setState({
-              isLoaded:true,
-              items:[json],
-                              
-          })
-      });
-  }
-    render(){
-      var { isLoaded,items}=this.state;  
-      const { selectedOption } = this.state;
-     
-      if(!isLoaded){
-        return <div><b>Loading....</b></div>
-    }
-    else{
-
-           return (
+   
+     render(){
+       
+         return (
              
-            <div className="App">
-               <Header/>
-            <div className='sidebar'>
+                    <div className="App">
+                          <Header/>
+                      <div className='sidebar'>
+                         <Sidebar/>
              
-              <div className="pageheading">
-             
-             <h2 >Overview Dashboard</h2>
-             </div> 
-              
-
-           <br></br>
-           {items.map(item=>(
-              <div key={items.id}>
-             <input  class="organisation" type="text"  placeholder="Organisation name" name="US Stell" value={item.address.city} readOnly/> 
-             <br></br> <br></br>
-             <Select 
-             defaultInputValue="Mysore"
-             value={selectedOption}
-             onChange={this.handleChange}
-             options={options}
-            />
-            </div>
-           ))}
-      <div className="cardview"> 
-        <Cardview/>
-      </div>
-      </div>  
-      <Footer/> 
-      </div> 
+                         <div className="cardview"> 
+                            <Cardview/>
+                         </div>
+                      </div>  
+                          <Footer/> 
+                     </div> 
       
-        );
+                 );
 
-    }
+              }
+  
   }
-}
 
 
 
