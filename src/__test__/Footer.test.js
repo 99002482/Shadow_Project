2f21c0a5fe4b51968bf9618995ae5880d6f991a6
99002482 <nisarga.hs@ltts.com>
2020-12-08 16:23:39 +0530

    formating

--- src/__test__/Footer.test.js
+++ src/__test__/Footer.test.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Footer from '../component/Footer';
-import {render} from "@testing-library/react";
+import React from "react";
+import ReactDom from "react-dom";
+import Footer from "../component/Footer";
+import { render } from "@testing-library/react";
 
-
-describe('Footer',()=>{
-it("renders Footer without crashing",()=>{
-    const div=document.createElement("div");
-    ReactDom.render(<Footer></Footer>,div);
-})
-it("Text fields working", () => {
-    const { getAllByText } = render(<Footer />);
-    getAllByText("©2020 EATON SHADOW PROJECT POC | All rights reserved | Terms Of Service | Privacy");
+describe("Footer", () => {
+  it("renders Footer without crashing", () => {
+    const div = document.createElement("div");
+    ReactDom.render(<Footer></Footer>, div);
   });
-
-
+  it("Text fields working", () => {
+    const { getAllByText } = render(<Footer />);
+    getAllByText(
+      "©2020 EATON SHADOW PROJECT POC | All rights reserved | Terms Of Service | Privacy"
+    );
+  });
 });
-
