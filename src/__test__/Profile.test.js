bdd961fc067a83481afbb5d4c0706d2f8b7fa981
99002482 <nisarga.hs@ltts.com>
2020-12-18 15:27:27 +0530

    removed issues

--- src/__test__/Profile.test.js
+++ src/__test__/Profile.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { render, screen } from "@testing-library/react";
+import { render} from "@testing-library/react";
 import Profile from "../component/Profile";
 
 describe("Profile", () => {
